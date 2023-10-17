c3a9769ef4bf4c540f84842c1068f0a5c6dcc4ff
zero <zero@192.168.1.15>
2023-10-17 12:28:03 +0700

    danhMuc

--- src/components/dungchung/Footer.js
+++ src/components/dungchung/Footer.js
@@ -9,39 +9,42 @@ const Footer = () => {
     const handleTrangChu = () => {
         dispath(offBack())
     }
+    const handleLinkActive = (event) => {
+        //add className="active vào giúp minh"
+    }
     return (
         <div id='footer'>
             <div className="container">
                 <div className="content">
-                    <NavLink to='/'>
+                    <NavLink to='/' activeClassName="active">
                         <div className="footerItem" onClick={handleTrangChu} >
                             <i className="fa-solid fa-house"></i>
                             <p>Trang chủ</p>
                         </div>
                     </NavLink>
 
-                    <NavLink to='/danh-muc'>
+                    <NavLink to='/danh-muc' activeClassName="active">
                         <div className="footerItem">
                             <i className="fa-solid fa-layer-group"></i>
                             <p>Danh mục</p>
                         </div>
                     </NavLink>
 
-                    <NavLink to='/thuong-hieu'>
+                    <NavLink to='/thuong-hieu' activeClassName="active">
                         <div className="footerItem">
                             <i className="fa-solid fa-bag-shopping"></i>
                             <p>Thương hiệu</p>
                         </div>
                     </NavLink>
 
-                    <NavLink to='/thong-bao'>
+                    <NavLink to='/thong-bao' activeClassName="active">
                         <div className="footerItem">
                             <i className="fa-solid fa-bell"></i>
                             <p>Thông báo</p>
                         </div>
                     </NavLink>
 
-                    <NavLink to='/tai-khoan'>
+                    <NavLink to='/tai-khoan' activeClassName="active">
                         <div className="footerItem">
                             <i className="fa-solid fa-user"></i>
                             <p>Tài khoản</p>
